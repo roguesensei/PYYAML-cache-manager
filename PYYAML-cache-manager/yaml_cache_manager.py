0a6277bd3927a7edcee671763bad1381470ab5ca
RogueSensei <chris.evans9417@protonmail.com>
2020-04-05 18:17:33 +0100

    Added method headers

--- PYYAML-cache-manager/yaml_cache_manager.py
+++ PYYAML-cache-manager/yaml_cache_manager.py
@@ -7,20 +7,25 @@ class YAMLCacheManager():
         self.path = path
 
     def cache(self, key, data):
+        '''Cache data in YAML format with assigned key'''
         cache_yaml(self.path, key, data)
 
     def get(self, key):
+        '''Get cached YAML data by key'''
         return get_yaml(self.path, key)
 
     def remove(self, key):
+        '''Remove cached YAML data by key'''
         remove_yaml(self.path, key)
 
 
 def cache_yaml(path, key, data):
+    '''Cache data in YAML format with assigned key'''
     with open('{}{}.yml'.format(legislate_path(path), key), 'w+') as cache_file:
         yaml.dump(data, cache_file, default_flow_style=False)
 
 def get_yaml(path, key):
+    '''Get cached YAML data by key'''
     try:
         data = yaml.load(open('{}{}.yml'.format(legislate_path(path), key)))
         return data
@@ -28,9 +33,11 @@ def get_yaml(path, key):
         return None
 
 def remove_yaml(path, key):
+    '''Remove cached YAML data by key'''
     os.remove('{}{}.yml'.format(legislate_path(path), key))
 
 def legislate_path(path):
+    '''Ensure path is in correct format'''
     if (path.startswith('/')):
         path = path[1:]
     if not (path.endswith('/')):
